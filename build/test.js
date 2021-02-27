a01ea02cfa629931a1e75fe8541d0d566b50743c
sjc0910 <sjc.0910@qq.com>
2021-02-27 17:07:00 +0800

    v1.1.4

---
+++ build/test.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var charextra = require("./index.js");
+console.log(charextra.fatal);
+console.log(charextra.error);
+console.log(charextra.warn);
+console.log(charextra.info);
